3de51839720a0d918d15d530a1f6c5a8dc910fb2
Vanya322 <vanya_kuzin_97@list.ru>
2021-12-15 08:58:40 +0300

    no comments...

--- client/src/app/services/member-services/cart/cart.service.ts
+++ client/src/app/services/member-services/cart/cart.service.ts
@@ -5,9 +5,9 @@ import {User, UserDto} from 'src/app/models/user.model'
 import { HttpClient } from "@angular/common/http";
 import { API_KEY } from "../../../utils/utils";
 import {UserService} from "../user/user.service";
-import { map } from 'rxjs/operators'
+
+import {debounceTime, map, tap, delay } from 'rxjs/operators'
 import { Product } from 'src/app/models/product.model';
-import {logger} from "codelyzer/util/logger";
 
 @Injectable({
   providedIn: 'root'
@@ -54,13 +54,35 @@ export class CartService {
 
   updateProduct(product: CartProduct) {
     this.observableCartProducts.subscribe((items) => {
-      const oldProduct = items.find((it) => it.id === product.id);
-      this.updateCountProductsInCart(1, (oldProduct?.countInCart || 0) < product.countInCart);
+      this.http.put(`${API_KEY}/carts/update/${this.user!.id}`,{
+        product,
+      })
+        .pipe(
+          delay(500),
+          tap(() => {
+            // const oldProduct = items.find((it) => it.id === product.id);
+            // if(oldProduct!.countInCart !== product.countInCart) {
+            //
+            //   console.log(oldProduct!.countInCart ,product.countInCart,oldProduct!.countInCart < product.countInCart);
+            //
+            //   let count: number = 0;
+            //
+            //   if(oldProduct!.countInCart < product.countInCart) {
+            //     count = product.countInCart - oldProduct!.countInCart;
+            //   } else {
+            //     count = oldProduct!.countInCart - product.countInCart;
+            //   }
+            //
+            //   this.updateCountProductsInCart(count, oldProduct!.countInCart < product.countInCart);
+            // }
+            const oldProduct = items.find((it) => it.id === product.id);
+            console.log(oldProduct!.countInCart ,product.countInCart,oldProduct!.countInCart < product.countInCart);
+            this.updateCountProductsInCart(1, oldProduct!.countInCart < product.countInCart);
+          })
+        )
+        .subscribe(() => {});
     })
 
-    this.http.put(`${API_KEY}/carts/update/${this.user!.id}`,{
-      product,
-    }).subscribe(() => {});
   }
 
   deleteProduct(product: CartProduct) {
